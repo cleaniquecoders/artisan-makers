5f94914e79055d20d801355604dcbd9ba0e8f804
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-01-26 07:31:50 +0800

    added Test

---
+++ tests/ArtisanCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CleaniqueCoders\ArtisanMakers\Tests;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+
+class ArtisanCommandTest extends TestCase
+{
+    /** @test */
+    public function it_has_artisan_makers()
+    {
+        $commands = [
+            'Contract',
+            'Exception',
+            'Model',
+            'Observer',
+            'Presenter',
+            'Processor',
+            'Service',
+            'Trait',
+            'Transformer',
+        ];
+
+        foreach ($commands as $command) {
+            $prefix  = 'TestArtisanMaker';
+            $name    = ($command == 'Model' ? 'Models/' . $prefix . $command : $prefix . $command);
+            $dir     = app_path(Str::plural($command));
+            $path    = $dir . '/' . $prefix . $command . '.php';
+            $message = $command . ' created successfully.';
+            $cmd     = 'make:' . Str::lower($command);
+
+            Artisan::call($cmd, ['name' => $name]);
+            $output = Artisan::output();
+            $this->assertContains($message, $output);
+            $this->assertFileExists($dir);
+            $this->assertFileExists($path);
+
+            $this->destroy($dir);
+
+            if ($command == 'Observer') {
+                $this->destroy(app_path('Providers'));
+            }
+
+            $this->assertFileNotExists($dir);
+            $this->assertFileNotExists($path);
+        }
+    }
+
+    private function destroy($path)
+    {
+        if (file_exists($path)) {
+            exec('rm -fr ' . $path);
+        }
+    }
+}
